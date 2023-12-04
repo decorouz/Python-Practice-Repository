44ce3d1119b805201e775d2a920cb84d85811d17
Biola <52200708+decorouz@users.noreply.github.com>
2023-12-04 02:12:21 +0100

    add edge case

--- solutions/min_number_jumps.py
+++ solutions/min_number_jumps.py
@@ -5,47 +5,58 @@ maximum number of steps that can be jumped going forward from that element.
 Write a function to return the minimum number of jumps you must take in
 order to get from the start to the end of the array.
 
-For example, given [6, 2, 4, 0, 5, 1, 1, 4, 2, 9], you should return 2, 
+For example, given [6, 6, 4, 0, 5, 1, 1, 4, 2, 9], you should return 2, 
 as the optimal solution involves jumping from 6 to 5, and then from 5 to 9.
 """
 
+# Think of the problem with an array of length 2
 
-def min_number_of_jumps(arr_num, arr_size):
-    """Return the minimum number of jumps.
 
-    Given a list [6, 2, 4, 0, 5, 1, 1, 4, 2, 9], you should return 2
+# Solution 1
+def min_jumps(arr: list[int]) -> int:
+    """Compute the minimum number of jumps
 
-    :param List: list of integers
-    :param n List: size of the list
-    :return int: the minimum number of jumps
+    Parameters
+    ----------
+    arr : list[int]
+        list of integers
+
+    Returns
+    -------
+    int
+        The maximum number of jumps
     """
-    if arr_size <= 1:
+
+    if not arr and len(arr) == 0:
         return 0
 
-    curr_max_reach = arr_num[0]
-    steps_count = arr_num[0]
-    jump = 0
+    max_reach = arr[0]  # maximum reachable index
+    steps = arr[0]
+    jumps = 1
 
-    for start in range(1, arr_size - 1):
-        curr_max_reach = max(curr_max_reach, start + arr_num[start])
-        steps_count = steps_count - 1
+    for start in range(1, len(arr)):
+        if start == len(arr) - 1:
+            return jumps
 
-        if steps_count == 0:
-            jump = jump + 1
-            steps_count = curr_max_reach - start
-    return jump + 1
+        max_reach = max(max_reach, start + arr[start])
+        steps -= 1
 
+        if steps == 0:
+            jumps += 1
+
+            if start >= max_reach:
+                return -1  # When nothing is reachable from a give source
+
+            steps = max_reach - start  # Number of steps without jumping
+    return -1
+
+
+# Driver program to test above function
 
 # Driver Program to test above function
-arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
 
-print(
-    "Minimum number of jumps to reach",
-    "end is",
-    min_number_of_jumps(arr, len(arr)),
-)
-
-
-# We are initially at the start of the array
-# Arr[i] = x, this mean that from index i, we can jump to the following i+1, i+2 ...,i+x
-# Aim is to return the minimum numbef of jumps
+if __name__ == "__main__":
+    arr1 = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
+    arr2 = [6, 2, 4, 0, 5, 0, 5, 1, 1, 4, 2, 0, 1, 0, 2, 9]
+    arr3 = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
+    print(min_jumps((arr2)))
